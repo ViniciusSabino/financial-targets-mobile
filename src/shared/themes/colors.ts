4abc487cc0322077c43439f872346d043be25754
Vinicius Rocha <vnc.chelsea@gmail.com>
2026-03-02 17:26:20 -0300

    style: define colors and more theme settings

---
+++ src/shared/themes/colors.ts
@@ -0,0 +1,25 @@
+export const lightColors = {
+  background: '#DDDDDD',
+  surface: '#FFFFFF',
+  primary: '#132522',
+  textPrimary: '#201F1F',
+  textSecondary: '#555555',
+  divider: '#B9C4BF',
+  success: '#1F7A4D',
+  error: '#C0392B',
+  warning: '#F39C12',
+  info: '#3498DB',
+};
+
+export const darkColors = {
+  background: '#201F1F',
+  surface: '#132522',
+  primary: '#B9C4BF',
+  textPrimary: '#DDDDDD',
+  textSecondary: '#B9C4BF',
+  divider: '#2C2C2C',
+  success: '#2ECC71',
+  error: '#E74C3C',
+  warning: '#F39C12',
+  info: '#3498DB',
+};
---
+++ src/shared/themes/typography.ts
@@ -0,0 +1,13 @@
+export const fonts = {
+  regular: 'MontserratRegular',
+  bold: 'MontserratBold',
+};
+
+export const fontSizes = {
+  h1: 32,
+  h2: 24,
+  h3: 20,
+  body: 16,
+  small: 14,
+  caption: 12,
+};
